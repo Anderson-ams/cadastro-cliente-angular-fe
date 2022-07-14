b51696747067cf9f16b34cee566858fcdec12ec8
Anderson Matos <ams.bns2021@gmail.com>
2022-07-14 11:19:08 -0300

    init-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'clientes' },
+  {
+    path: 'clientes',
+    loadChildren: () => import('./client/client.module').then(m => m.ClientModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
